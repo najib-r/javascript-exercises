4c734485f51d4ccb2406162220b64a8711a94ecd
ysosrss <najib72645@gmail.com>
2022-10-24 10:40:55 +0800

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    } 
+    else if (typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }   
+        return sum;    
+    }
+    else {
+        for (let i = end; i <= start; i++) {
+            sum += i;
+        }   
+        return sum;    
+    }
+    
 };
 
 // Do not edit below this line

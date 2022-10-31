f40f5151c1cae7b83000793f8ed7262f2f9ff115
ysosrss <najib72645@gmail.com>
2022-10-31 18:14:53 +0800

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const withoutCommas = string.toLowerCase().replace(/[^a-z]/g, '');
+    var splitString = withoutCommas.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray === withoutCommas;
 };
 
 // Do not edit below this line

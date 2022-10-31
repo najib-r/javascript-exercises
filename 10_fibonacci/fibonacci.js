30b7750ed36737bf627759e2f59d34072c7d1340
ysosrss <najib72645@gmail.com>
2022-10-31 18:25:52 +0800

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    array = [1,1];
+    for (i=2;i<50;i++) {
+        num=array[i-1]+array[i-2];
+        array.push(num);
+    }
+    if (n>=0){
+        return array[n-1];
+    } 
+    else {
+        return "OOPS";
+    }
+    
 };
 
 // Do not edit below this line
